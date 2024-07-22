2045ab8e04e7fe029013551b7cdfaf9c363be3ba
Bezerra <54821094+jpbezerra@users.noreply.github.com>
2024-07-22 00:21:17 -0300

    Add files via upload

---
+++ hello-world.asm
@@ -0,0 +1,12 @@
+.data
+	# área para dados na memória principal
+	msg: .asciiz "Olá, mundo!" # mensagem de olá mundo
+
+
+.text
+	# área para instruções do programa
+	
+	li $v0, 4 # impressão de string
+	la $a0, msg # indicar o endereço em que está a mensagem
+	 
+	syscall # faça o que está em $a0
