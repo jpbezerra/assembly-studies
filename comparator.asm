2c1d54b3a610da3c12bc6f5a113b9c4b962e1925
Bezerra <54821094+jpbezerra@users.noreply.github.com>
2024-07-22 11:56:13 -0300

    Update comparator.asm

--- comparator.asm
+++ comparator.asm
@@ -1,44 +1,60 @@
 .data
-	msg: .asciiz "Digite um número: "
-	msg_maior: "Este número é maior que 0."
-	msg_menor: "Este número é menor que 0."
-	msg_igual: .asciiz "Este número é o próprio 0."
+	msg: .asciiz "Digite um nÃºmero: "
+	msg_maior: "Este nÃºmero Ã© maior que 0."
+	msg_menor: "Este nÃºmero Ã© menor que 0."
+	msg_igual: .asciiz "Este nÃºmero Ã© o prÃ³prio 0."
 	
 .text
+	# imprimir msg
 	li $v0, 4
 	la $a0, msg
 	syscall
 	
+	# ler inteiro
 	li $v0, 5
 	syscall
 	
+	# guardando o inteiro em $t0
 	move $t0, $v0
 	
+	# se $t0 > 0
 	bgt $t0, $zero, label_maior
+	
+	# se $t0 < 0
 	blt $t0, $zero, label_menor
+	
+	# se $t0 == 0
 	beq $t0, $zero, label_igual
 	
 label_maior:
+	# imprimir msg_maior
 	li $v0, 4
 	la $a0, msg_maior
 	syscall
 	
+	# facilitador para ir para a label forever
 	beqz $zero, forever
 
 label_menor:
+	# imprimir msg_menor
 	li $v0, 4
 	la $a0, msg_menor
 	syscall
 	
+	# facilitador para ir para a label forever
 	beqz $zero, forever
 
 label_igual:
+	# imprimir msg_igual
 	li $v0, 4
 	la $a0, msg_igual
 	syscall
 	
+	# facilitador para ir para a label forever
 	beqz $zero, forever
 
+# label de exit, apenas para facilitar e escrever menos cÃ³digo
 forever:
+	# exit
 	li $v0, 10
 	syscall
